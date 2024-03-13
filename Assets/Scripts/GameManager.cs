913fcca12d6f3eeda1db5da205c3f3c2de51843e
AaronLWilliams <williamsal1990@gmail.com>
2024-03-13 17:56:43 -0500

    Fixed bug with health ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,21 +7,24 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    public float health;
+    public int health;
     public int ammo;
     public int maxAmmo;
     public TMP_Text AmmoText;
     public TMP_Text HealthText;
+    public GameObject player;
+
     List<Zombie> enemies = new List<Zombie>();
 
     private void Awake()
     {
-        enemies = GameObject.FindObjectsOfType<Zombie>().ToList();
+        //enemies = GameObject.FindObjectsOfType<Zombie>().ToList();
 
     }
 
     private void Update()
     {
+        health = player.GetComponent<health>().currentHealth;
         AmmoText.text = ammo + "/" + maxAmmo;
         HealthText.text = "Health: " + health;
     }
