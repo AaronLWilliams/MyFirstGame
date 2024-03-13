88d4f01fc44f7188af1b796ca94b59b06d25cf72
AaronLWilliams <williamsal1990@gmail.com>
2024-03-13 17:32:49 -0500

    Added ability to switch weapons

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,9 +4,69 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public int currentWeapon = 0;
+
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    void Update()
+    {
+        int previousSelectedWeapon = currentWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+                currentWeapon = 0;
+            else
+                currentWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (currentWeapon <= 0f)
+                currentWeapon = transform.childCount - 1;
+            else
+                currentWeapon--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            currentWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            currentWeapon = 2;
+        }
+
+        if (previousSelectedWeapon != currentWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == currentWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
 
     public void Fire()
     {
